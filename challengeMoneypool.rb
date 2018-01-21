85e0e9d4c324a09eb534328968a83bd9f33e262a
DniGaytan <dnigaytan07@gmail.com>
2018-01-21 03:04:16 -0600

    challengeMoneypool added as an xml analyzer for macbeth.xml

---
+++ challengeMoneypool.rb
@@ -0,0 +1,50 @@
+#Ruby program that parses an internet xml file and check how many lines
+#each character has
+#Daniel Gaytan dnigaytan07@gmail.com
+
+require 'nokogiri'
+require 'open-uri'
+
+#speaker_hash was used in order to avoid a two dimensional array declaration
+def set_speaker_hash(speakers)
+	speaker_hash = Hash.new
+
+	speakers.each do |speaker|
+		#ignore the speech tag that contains the ALL text within the SPEAKER tag
+		if speaker.text == "ALL"
+			next
+		end
+		#sets the speaker key value to 0 for further additions
+		speaker_hash[speaker.text] = 0
+	end
+	return speaker_hash
+end
+
+
+def count_lines(speaker_hash,speeches)
+	speeches.each do |speech|
+		#Grabs all the lines that speech tag has itself
+		lines =  speech.css("LINE")
+		#sum the lines of the current speech to the speaker key value
+		speaker_hash[speech.css("SPEAKER").text.to_s] = speaker_hash[speech.css("SPEAKER").text.to_s].to_i + lines.length()
+	end
+	return speaker_hash
+end
+
+#reads the whole xml file 
+xml_file = Nokogiri::XML(open("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"))
+
+#grabs all the speaker tags
+speakers = xml_file.css("SPEAKER")
+
+speaker_hash = set_speaker_hash(speakers)
+
+#grabs all the speech tags and their children
+speeches = xml_file.css("SPEECH")
+
+speaker_hash = count_lines(speaker_hash,speeches)
+
+#print out the all keys and values of speaker_hash
+speaker_hash.to_a.each do |speaker|
+	puts "#{speaker[1]} => #{speaker[0]}" + "\n"
+end
