770bf6c34d8ef9a9ef223aa65e3698388a3acb06
DniGaytan <dnigaytan07@gmail.com>
2018-01-21 21:32:04 -0600

    Erased set_speaker_hash for optimal purposes

--- challengeMoneypool.rb
+++ challengeMoneypool.rb
@@ -5,21 +5,6 @@
 require 'nokogiri'
 require 'open-uri'
 
-#speaker_hash was used in order to avoid a two dimensional array declaration
-def set_speaker_hash(speakers)
-	speaker_hash = Hash.new
-
-	speakers.each do |speaker|
-		#ignore the speech tag that contains the ALL text within the SPEAKER tag
-		if speaker.text == "ALL"
-			next
-		end
-		#sets the speaker key value to 0 for further additions
-		speaker_hash[speaker.text] = 0
-	end
-	return speaker_hash
-end
-
 def count_lines(speaker_hash,speeches)
 	speeches.each do |speech|
 		#Grabs all the lines that speech tag has itself
@@ -33,10 +18,7 @@ end
 #reads the whole xml file 
 xml_file = Nokogiri::XML(open("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"))
 
-#grabs all the speaker tags
-speakers = xml_file.css("SPEAKER")
-
-speaker_hash = set_speaker_hash(speakers)
+speaker_hash = Hash.new(0)
 
 #grabs all the speech tags and their children
 speeches = xml_file.css("SPEECH")
